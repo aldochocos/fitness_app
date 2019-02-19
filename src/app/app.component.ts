4bfd21c6ec22184d49239ebf1b0d557836d39811
aldochocos <aldo.chocos@pucp.pe>
2019-02-19 06:16:44 -0500

    initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fitness-app';
+
+  constructor() { }
+
+  ngOnInit() {
+
+
+  }
+
+  /* 
+    steps:
+    security
+    app header
+    app nav
+
+  */
+  
 }
---
+++ src/labs/greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "hello" + person;
+}
+var user = "Aldo Chocos";
+document.body.innerHTML = greeter(user);
